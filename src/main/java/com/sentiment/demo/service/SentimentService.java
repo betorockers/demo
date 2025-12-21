14e83c5ecc1ca364d7eb1f639299b545e04114db
bdidominguez <bdidominguez@gmail.com>
2025-12-20 21:38:10 -0300

    corrigiendo nombre de metodo predict(), integre con properties, ajuste SentimentService

--- src/main/java/com/sentiment/demo/service/SentimentService.java
+++ src/main/java/com/sentiment/demo/service/SentimentService.java
@@ -1,19 +1,51 @@
 package com.sentiment.demo.service;
 
-import com.sentiment.demo.client.SentimentDsClient;
 import com.sentiment.demo.dto.SentimentResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Service
 public class SentimentService {
-    private final SentimentDsClient client;
+    private final RestTemplate restTemplate;
+    @Value("${sentiment.mode}")
+    private String mode;
 
-    public SentimentService(SentimentDsClient client) {
-        this.client = client;
+    @Value("${python.api.url}")
+    private String pythonUrl;
+
+    public SentimentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
-    public SentimentResponse analizar(String texto){
-        var dsResponseMock = client.predict(texto);
-        return new SentimentResponse(dsResponseMock.getPrevision(), dsResponseMock.getProbabilidad());
+    public SentimentResponse predict(String text) {
+
+        if (mode.equalsIgnoreCase("mock")) {
+            return new SentimentResponse("Positivo", 0.95);
+        }
+
+        try {
+
+            Map<String,Object> response =
+                    restTemplate.postForObject(
+                            pythonUrl,
+                            Map.of("text", text),
+                            Map.class
+                    );
+
+            return new SentimentResponse(
+                    response.get("label").toString(),
+                    ((Number)response.get("probability")).doubleValue()
+            );
+
+        } catch (Exception ex) {
+
+            return new SentimentResponse(
+                    "Error: Modelo no disponible",
+                    0.0
+            );
+        }
     }
 }
