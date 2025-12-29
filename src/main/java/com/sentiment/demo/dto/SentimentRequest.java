f13c368f2bc228f993ae3d41d3851f8262959205
betorock toledo <omar.toledus@gmail.com>
2025-12-29 10:44:01 -0300

    Dev 4: Tests automáticos exitosos (6/6) y README actualizado para jurado

--- src/main/java/com/sentiment/demo/dto/SentimentRequest.java
+++ src/main/java/com/sentiment/demo/dto/SentimentRequest.java
@@ -14,7 +14,6 @@ import jakarta.validation.constraints.Size;
 public record SentimentRequest(
         @JsonProperty("text")
         @NotBlank(message = "El texto no puede estar vacío")
-        @Size(min = 5, max = 2000, message = "El texto debe tener al entre 5 y 2000 caracteres")
         @Size(min = 5, max = 2000, message = "El texto debe tener entre 5 y 2000 caracteres")
         String text
                                )
